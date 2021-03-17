03773a508172fe268d0f9bb69732d6e6782a0da0
erickmx <erickblanc96@gmail.com>
2021-03-17 01:58:09 -0600

    fix(ui): removed unnecessary ui package

--- packages/ui/lib/components/Title.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-interface ITitle {
-    hasBorder: boolean;
-}
-declare const Title: import("@emotion/styled").StyledComponent<{
-    theme?: import("@emotion/react").Theme | undefined;
-    as?: import("react").ElementType<any> | undefined;
-} & ITitle, import("react").DetailedHTMLProps<import("react").HTMLAttributes<HTMLHeadingElement>, HTMLHeadingElement>, {}>;
-export default Title;
