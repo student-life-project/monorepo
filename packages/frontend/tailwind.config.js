0b38ffee93a4617a48eea1596550192874f56809
Erick Mejia Blanco <erickblanc96@gmail.com>
2021-04-03 02:04:27 -0600

    feact(front): added support for custom tailwind classes and svg (#7)

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -2,9 +2,23 @@ const colors = require('tailwindcss/colors');
 
 module.exports = {
   darkMode: 'class',
+  purge: [
+    'src/**/*.js',
+    'src/**/*.jsx',
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    'public/**/*.html',
+  ],
   theme: {
     extend: {
       colors,
+    },
+    colors: {
+      crazy: '#543fea',
+    },
+    fontFamily: {
+      maven: ['Maven Pro', 'Helvetica', 'Arial', 'sans-serif'],
+      montserrat: ['Montserrat', 'Helvetica', 'Arial', 'sans-serif']
     },
   },
   variants: {},
