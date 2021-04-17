df7d62f194737d72a171f7ed7e14a6c48d0c1b46
Erick Mejia Blanco <erickblanc96@gmail.com>
2021-04-16 23:41:35 -0500

    feat(frontend): custom error page (#10)
    * feat(frontend): implemented prototype of error page
    
    * feat(frontend): implemented link redirection to home in error page

---
+++ packages/frontend/src/pages/_error.tsx
@@ -0,0 +1,65 @@
+import { NextPage } from 'next';
+import Link from 'next/link';
+import xw from 'xwind';
+
+import Button from '@/components/Button';
+
+export interface IError {
+  statusCode: number;
+  message: string;
+}
+
+const Error: NextPage<IError> = ({ statusCode, message }) => {
+  return (
+    <div
+      css={xw`max-w-screen-xl flex items-center justify-center w-full h-screen px-4 lg:px-0 md:mx-auto`}
+    >
+      <div css={xw`w-full h-3/4 flex flex-col md:flex-row md:h-1/2`}>
+        <img
+          src="/images/error.jpg"
+          alt="error_image"
+          css={xw`w-full h-1/2 bg-gray-400 md:w-1/2 md:h-full`}
+        />
+        <section
+          css={xw`w-full h-1/2 flex flex-col justify-around mt-4 text-center md:w-1/2 md:h-full md:mt-0`}
+        >
+          <h1 css={xw`text-4xl font-bold font-maven`}>Error {statusCode}</h1>
+          <p css={xw`font-montserrat break-words`}>{message}</p>
+          <Link href="/">
+            <Button FPrimary css={xw`w-full self-center lg:w-1/2`}>
+              <span css={xw`mx-2 w-full`}>Ir al inicio</span>
+            </Button>
+          </Link>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+Error.getInitialProps = ({ res, err }) => {
+  let statusCode = 0;
+  let message = '';
+
+  if (res) {
+    statusCode = res.statusCode;
+    switch (statusCode) {
+      case 404:
+        message = 'Pagina no encontrada';
+        break;
+      default:
+        message = 'Error inesperado';
+    }
+  }
+
+  if (err) {
+    statusCode = err.statusCode || 404;
+    message = err.message || '';
+  }
+
+  return {
+    statusCode: statusCode || 404,
+    message,
+  };
+};
+
+export default Error;
