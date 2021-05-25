6b93f3a37e51e79c4356a928cc5fc388abf572e5
Erev14 <erevjoach14@gmail.com>
2021-05-24 19:43:31 -0500

    feat(backend): auth env var base usage, v1/api global prefix, allow diff host communication (#15)

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/v1/api');
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: true,
+    }),
+  );
   await app.listen(3010);
 }
 bootstrap();
