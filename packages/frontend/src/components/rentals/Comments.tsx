c9e0b9ebbb2f066c79f0136cd0b0bc7c36f8e630
AlfredoCU <alfredo_11_cu@hotmail.com>
2022-05-14 23:55:40 -0500

    feat: logic - comment form created

--- packages/frontend/src/components/rentals/Comments.tsx
+++ packages/frontend/src/components/rentals/Comments.tsx
@@ -1,7 +1,10 @@
 import { faBullhorn, faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { FC } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import xw from 'xwind';
+
+import { ErrorMessageInput, NameInput } from '@/constants';
 
 import Anchor from '../common/Anchor';
 import Button from '../common/Button';
@@ -14,10 +17,31 @@ type TComments = {
   isLogedIn: boolean;
 };
 
+interface ICommentData {
+  comment: string;
+}
+
 const Comments: FC<TComments> = ({ comments, isLogedIn }) => {
   // TODO: Need to implement
   // Ver comentarios sólo si existe una sesión iniciada.
   // Las opciones de editar y eliminar solo son para el owner.
+
+  const {
+    handleSubmit,
+    register,
+    reset,
+    watch,
+    formState: { errors },
+  } = useForm({ mode: 'all' });
+
+  const comment = watch('comment');
+  const onSubmit: SubmitHandler<ICommentData> = async (data) => {
+    // eslint-disable-next-line no-console
+    console.log(data);
+    reset();
+    // Despues de enviar el comentario. enviarle un id de user.
+    // Para obetener id, name, userImage, comment y date.
+  };
 
   return (
     <>
@@ -25,27 +49,27 @@ const Comments: FC<TComments> = ({ comments, isLogedIn }) => {
         <div css={xw`w-full`}>
           <h2 css={xw`w-full py-7 text-xl font-bold`}>Comentarios</h2>
 
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <Textarea
               id="comment"
               maxLength={255}
-              // counter={rentalPlace}
+              counter={comment?.length}
               placeholder="Escribe un comentario..."
-              // register={{
-              //   ...register('rentalPlace', {
-              //     required: ErrorMessageInput.inputRequire(NameInput.rentalPlace),
-              //     maxLength: {
-              //       value: 500,
-              //       message: ErrorMessageInput.max(500),
-              //     },
-              //   }),
-              // }}
-              // error={errors.rentalPlace}
-              // messageError={errors.rentalPlace?.message}
+              register={{
+                ...register('comment', {
+                  required: ErrorMessageInput.inputRequire(NameInput.comment),
+                  maxLength: {
+                    value: 500,
+                    message: ErrorMessageInput.max(500),
+                  },
+                }),
+              }}
+              error={errors.comment}
+              messageError={errors.comment?.message}
             />
 
             <DoubleSpace classNames={xw`sm:justify-end sm:gap-4 sm:mt-2`}>
-              <Button type="button" BDanger>
+              <Button type="button" BDanger onClick={() => reset()}>
                 Cancelar
               </Button>
               <Button type="submit" FPrimary css={xw`my-2 sm:my-0`}>
