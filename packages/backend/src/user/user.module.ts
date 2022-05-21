64b94d7a956d7e83ff9465f86bb3bb4d18bb8bbb
Erev14 <erevjoach14@gmail.com>
2022-05-21 09:42:30 +0000

    fix(backend): remove unused imports feat(backend): add axios import

--- packages/backend/src/user/user.module.ts
+++ packages/backend/src/user/user.module.ts
@@ -1,26 +1,30 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 
-import { ImageModule } from '../image/image.module';
-import { MessageModule } from '../message/message.module';
-import { OwnerModule } from '../owner/owner.module';
-import { ReportModule } from '../report/report.module';
-import { StudentModule } from '../student/student.module';
-import { User, UserSchema } from './user.schema';
+// import { MongooseModule } from '@nestjs/mongoose';
+// import { ImageModule } from '../image/image.module';
+// import { MessageModule } from '../message/message.module';
+// import { OwnerModule } from '../owner/owner.module';
+// import { ReportModule } from '../report/report.module';
+// import { StudentModule } from '../student/student.module';
+// import { User, UserSchema } from './user.schema';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-    ImageModule,
-    MessageModule,
-    OwnerModule,
-    ReportModule,
-    StudentModule,
+    // MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    // ImageModule,
+    // MessageModule,
+    // OwnerModule,
+    // ReportModule,
+    // StudentModule,
+    HttpModule,
   ],
   exports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    // MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [UserController],
+  providers: [UserService],
 })
 export class UserModule {}
