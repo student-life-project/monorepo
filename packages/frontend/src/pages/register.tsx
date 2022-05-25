b0ad388b52cb113b8b65f69890e5876e9f671549
AlfredoCU <alfredo_11_cu@hotmail.com>
2022-05-24 21:45:06 -0500

    feat: register view created

---
+++ packages/frontend/src/pages/register.tsx
@@ -0,0 +1,210 @@
+/* eslint-disable simple-import-sort/imports */
+import xw from 'xwind';
+import styled from '@emotion/styled';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { EUserType } from '@student_life/common';
+import { NextPage } from 'next';
+import { useEffect } from 'react';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+
+import Button from '@/components/common/Button';
+import CenteredBody from '@/components/common/CenteredBody';
+import DoubleFormSpace from '@/components/common/DoubleFormSpace';
+import Input from '@/components/common/Input';
+import Label from '@/components/common/Label';
+import Radio from '@/components/common/Radio';
+import Tooltip from '@/components/common/Tooltip';
+import { ErrorMessageInput, NameInput } from '@/constants';
+import { CalculateAge } from '@/utils/calculateAge';
+
+interface IRegisterData {
+  userType: EUserType;
+  firstName: string;
+  lastName: string;
+  birthDate: string;
+}
+
+const Form = styled.form`
+  ${xw`
+    px-4
+    w-full
+    lg:w-6/12
+  `}
+`;
+
+const Title = styled.h1`
+  ${xw`
+    mb-4
+    w-full
+    text-4xl
+    text-center
+    font-medium
+    sm:text-5xl
+  `}
+`;
+
+const Text = styled.p`
+  ${xw`
+    mb-4
+    text-lg
+    font-bold
+    text-center
+  `}
+`;
+
+const RadioContainer = styled.div`
+  ${xw`
+    h-12
+    px-4
+    mb-4
+    border
+    rounded
+    border-gray-200
+  `}
+`;
+
+const Register: NextPage = () => {
+  const {
+    handleSubmit,
+    register,
+    control,
+    reset,
+    formState: { errors },
+  } = useForm({
+    mode: 'all',
+  });
+
+  useEffect(() => {
+    reset({ userType: EUserType.STUDENT });
+  }, [reset]);
+
+  const onSubmit: SubmitHandler<IRegisterData> = async (data) => {
+    // eslint-disable-next-line no-console
+    console.log(data);
+  };
+
+  return (
+    <CenteredBody css={xw`h-auto lg:h-screen py-10`}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Title>Te damos la bienvenida</Title>
+        <Text>
+          Es importante que ingreses la siguiente información para personalizar
+          tu perfil
+        </Text>
+
+        <DoubleFormSpace>
+          <Controller
+            name="userType"
+            control={control}
+            rules={{ required: true }}
+            render={({ field: { onChange, value } }) => {
+              const role = parseInt(value, 10);
+
+              return (
+                <>
+                  <RadioContainer data-tip data-for="student">
+                    <>
+                      <Radio
+                        name="student"
+                        label="Estudiante"
+                        value={EUserType.STUDENT}
+                        onChange={onChange}
+                        checked={role === EUserType.STUDENT}
+                      />
+
+                      <Tooltip id="student" type="info">
+                        <span>Buscar alojamientos</span>
+                      </Tooltip>
+                    </>
+                  </RadioContainer>
+
+                  <RadioContainer data-tip data-for="renter">
+                    <>
+                      <Radio
+                        name="renter"
+                        label="Arrendatario"
+                        value={EUserType.OWNER}
+                        onChange={onChange}
+                        checked={role === EUserType.OWNER}
+                      />
+
+                      <Tooltip id="renter" type="info">
+                        <span>Buscar inquilino o buscar un rommie</span>
+                      </Tooltip>
+                    </>
+                  </RadioContainer>
+                </>
+              );
+            }}
+          />
+
+          <div css={xw`mb-4`}>
+            <Label id="label-first-name" htmlFor="first-name">
+              {NameInput.firstName}
+            </Label>
+            <Input
+              type="text"
+              id="first-name"
+              placeholder="Tu nombre"
+              register={{
+                ...register('firstName', {
+                  required: ErrorMessageInput.inputRequire(NameInput.firstName),
+                }),
+              }}
+              error={errors.firstName}
+              messageError={errors.firstName?.message}
+            />
+          </div>
+
+          <div css={xw`mb-4`}>
+            <Label id="label-last-name" htmlFor="last-name">
+              {NameInput.lastName}
+            </Label>
+            <Input
+              type="text"
+              id="last-name"
+              placeholder="Tu apellido"
+              register={{
+                ...register('lastName', {
+                  required: ErrorMessageInput.inputRequire(NameInput.lastName),
+                }),
+              }}
+              error={errors.lastName}
+              messageError={errors.lastName?.message}
+            />
+          </div>
+        </DoubleFormSpace>
+
+        <div css={xw`mb-4`}>
+          <Label id="label-birth-date" htmlFor="birth-date">
+            {NameInput.birthDate}
+          </Label>
+          <Input
+            type="date"
+            id="birth-date"
+            placeholder="Tu fecha de nacimiento"
+            register={{
+              ...register('birthDate', {
+                required: ErrorMessageInput.inputRequire(NameInput.birthDate),
+                validate: (value) =>
+                  CalculateAge(value) >= 18 || ErrorMessageInput.ageValid,
+              }),
+            }}
+            error={errors.birthDate}
+            messageError={errors.birthDate?.message}
+          />
+        </div>
+
+        <div css={xw`my-4`}>
+          <Button type="submit" FPrimary css={xw`w-full`}>
+            <span css={xw`mr-2`}>Regístrate</span>
+            <FontAwesomeIcon icon={faChevronRight} height=".875rem" />
+          </Button>
+        </div>
+      </Form>
+    </CenteredBody>
+  );
+};
+
+export default Register;
