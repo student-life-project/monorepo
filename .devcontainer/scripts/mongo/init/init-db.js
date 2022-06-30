92bdf8c8605b5f08cf1b08843667439b47f28707
Erev14 <erevjoach14@gmail.com>
2022-06-30 21:13:52 +0000

    fix(backend): devcontainer

--- .devcontainer/scripts/mongo/init/init-db.js
+++ .devcontainer/scripts/mongo/init/init-db.js
@@ -1,3 +1,3 @@
 // Create DB and collection
 db = new Mongo().getDB("student-life");
-db.createCollection("rental-place", { capped: false });
+db.createCollection("rentalplaces", { capped: false });
