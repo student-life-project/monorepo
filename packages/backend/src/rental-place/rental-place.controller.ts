558b290167c54ad73037f2f70cb09204fc8e0e05
Erev14 <erevjoach14@gmail.com>
2022-07-01 07:42:09 +0000

    feat(backend): crud rental place chore(back): add no param reassign

--- packages/backend/src/rental-place/rental-place.controller.ts
+++ packages/backend/src/rental-place/rental-place.controller.ts
@@ -5,65 +5,81 @@ import {
   Get,
   NotFoundException,
   Param,
+  Patch,
   Post,
   Put,
+  Req,
+  Res,
+  UnsupportedMediaTypeException,
+  UploadedFiles,
+  UseInterceptors,
 } from '@nestjs/common';
+import { FilesInterceptor } from '@nestjs/platform-express';
 import {
+  ApiBody,
+  ApiConsumes,
   ApiCreatedResponse,
   ApiNotFoundResponse,
   ApiOkResponse,
   ApiTags,
 } from '@nestjs/swagger';
+// import { zip } from 'express-zip';
+import { Response } from 'express';
+import { createReadStream } from 'fs';
 
 import { AddressService } from '../address/address.service';
+import { Auth } from '../auth.decorator';
+import {
+  asyncFilter,
+  getFullPath,
+  isSafeFileExtension,
+  saveImageToStorage,
+} from '../config/multer.config';
+import { FilesUploadDto } from '../helper/dto/file-upload.dto';
+import { UserNotAllowOrOwnerException } from '../helper/exceptions/user-not-allowed-to-update-data';
 import { ImageService } from '../image/image.service';
-// import { RateService } from '../rate/rate.service';
-import { RuleService } from '../rule/rule.service';
+import { RateService } from '../rate/rate.service';
+import { UserService } from '../user/user.service';
 import { CreateRentalPlaceDto } from './dto/create-rental-place.dto';
 import { UpdateRentalPlaceDto } from './dto/update-rental-place.dto';
 import { RentalPlaceService } from './rental-place.service';
 
+/* eslint no-underscore-dangle: 0 */
 @ApiTags('rental-place')
 @Controller('rental-place')
 export class RentalPlaceController {
+  IMAGES_PATH = './uploads/rental-places';
+
   // eslint-disable-next-line no-useless-constructor
   constructor(
     private readonly rentalPlaceService: RentalPlaceService,
     private readonly addressService: AddressService,
-    private readonly ruleService: RuleService,
-    private readonly imageService: ImageService, // private readonly rateService: RateService,
+    private readonly imageService: ImageService,
+    private readonly userService: UserService,
+    private readonly rateService: RateService,
   ) {}
 
-  // @ApiBearerAuth()
   @ApiCreatedResponse({
     description: 'Rental place creation just for admins or lessors',
+    // type: CreateRentalPlaceDto
   })
   @Post()
-  async create(@Body() createRentalPlaceDto: CreateRentalPlaceDto) {
-    // TODO confirm rental-place/ POST works
-    // create rules
-    const rulesIds = await this.ruleService.createMany(
-      createRentalPlaceDto.rules,
-    );
-    // eslint-disable-next-line no-param-reassign
-    createRentalPlaceDto.rules = rulesIds;
-
-    // create images
-    const imagesIds = await this.imageService.createMany(
-      createRentalPlaceDto.images,
-    );
-    // eslint-disable-next-line no-param-reassign
-    createRentalPlaceDto.images = imagesIds;
-
-    // create address
+  @Auth('create:rental-place')
+  async create(
+    @Body() createRentalPlaceDto: CreateRentalPlaceDto,
+    @Req() req: any,
+  ) {
+    // // create address
     const addressId = await this.addressService.create(
       createRentalPlaceDto.address,
     );
-    // eslint-disable-next-line no-param-reassign
-    createRentalPlaceDto.address = addressId;
 
-    // create rentalPlace
-    return this.rentalPlaceService.create(createRentalPlaceDto);
+    // // create rentalPlace
+    return this.rentalPlaceService.create({
+      ...createRentalPlaceDto,
+      owner: req.user.sub,
+      address: addressId,
+    });
   }
 
   @ApiOkResponse({ description: 'Find all the rental places' })
@@ -76,6 +92,7 @@ export class RentalPlaceController {
   @ApiNotFoundResponse({ description: 'Rental place does not exists' })
   @ApiOkResponse({ description: 'Get a rental place by id' })
   @Get(':id')
+  // @Auth('read:rental-place')
   async findOne(@Param('id') id: string) {
     // TODO make sure retrive all need info rentals/:id GET
     const rentalPlace = await this.rentalPlaceService.findOne(id);
@@ -91,28 +108,142 @@ export class RentalPlaceController {
     description: 'Rental place does not exists, not able to Update',
   })
   @Put(':id')
+  @Auth('update:rental-place')
   async update(
     @Param('id') id: string,
     @Body() updateRentalPlaceDto: UpdateRentalPlaceDto,
+    @Req() req: any,
   ) {
-    const rentalPlace = await this.rentalPlaceService.update(
-      id,
-      updateRentalPlaceDto,
-    );
-    if (!rentalPlace)
+    const rentalPlace = await this.rentalPlaceService.findOne(id);
+    if (!rentalPlace) {
       throw new NotFoundException(
         'Rental Place does not exists, not able to Update',
       );
+    }
+    if (!this.userService.isUserAllowed(rentalPlace.owner, req.user)) {
+      throw new UserNotAllowOrOwnerException();
+    }
+    // delete pass address and rates
+    if (updateRentalPlaceDto.address) {
+      await this.addressService.update(
+        updateRentalPlaceDto.address._id ?? '',
+        updateRentalPlaceDto.address,
+      );
+    }
+    await this.rateService.deleteByPlaceId(id);
+    if (updateRentalPlaceDto.rates) {
+      updateRentalPlaceDto.rates = await this.rateService.createMany(
+        updateRentalPlaceDto.rates,
+      );
+    }
+    await this.rentalPlaceService.update(id, updateRentalPlaceDto);
     return rentalPlace;
   }
 
   @ApiOkResponse({ description: 'Delete a rental place by id' })
   @ApiNotFoundResponse({ description: 'Rental place does not exists' })
   @Delete(':id')
-  async remove(@Param('id') id: string) {
-    const rentalPlace = await this.rentalPlaceService.remove(id);
+  @Auth('delete:rental-place')
+  async remove(@Param('id') id: string, @Req() req: any) {
+    const rentalPlace = await this.rentalPlaceService.findOne(id);
     if (!rentalPlace)
       throw new NotFoundException('Rental place does not exists');
-    return rentalPlace;
+
+    if (!this.userService.isUserAllowed(rentalPlace.owner, req.user)) {
+      throw new UserNotAllowOrOwnerException();
+    }
+    // delete from disk last images
+    this.rentalPlaceService.removeFiles(rentalPlace.images);
+    // remove on mongo
+    this.imageService.deleteByPlaceId(id);
+    this.addressService.deleteByPlaceId(rentalPlace.address.id ?? '');
+    this.rateService.deleteByPlaceId(id);
+    const rentalPlaceDeleted = await this.rentalPlaceService.remove(id);
+
+    return rentalPlaceDeleted;
+  }
+
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'Upload rental place images',
+    type: FilesUploadDto,
+  })
+  @UseInterceptors(FilesInterceptor('files', 32, saveImageToStorage))
+  @Patch(':id/upload')
+  @Auth('upload-image:rental-place')
+  async uploadFile(
+    @Param('id') id: string,
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Req() req: any,
+  ) {
+    const fileNames = files?.map((file) => file.filename);
+
+    if (!fileNames)
+      throw new UnsupportedMediaTypeException('File must be a png, jpg/jpeg');
+
+    // delete from disk last images
+    const rentalPlace = await this.rentalPlaceService.findOne(id);
+    // validate rental place
+    if (!rentalPlace) {
+      this.rentalPlaceService.removeFiles(files);
+      // files.map((file) => removeFile(file.filename));
+      throw new NotFoundException('Rental place does not exists');
+    }
+    // validate has access to update
+    if (!this.userService.isUserAllowed(rentalPlace.owner, req.user)) {
+      // files.map((file) => removeFile(file.filename));
+      this.rentalPlaceService.removeFiles(files);
+      throw new UserNotAllowOrOwnerException();
+    }
+    this.rentalPlaceService.removeFiles(rentalPlace.images);
+    this.imageService.deleteByPlaceId(id);
+
+    // separate safe and unsafe files to prevent change extension vulnerability
+    const safeFiles = await asyncFilter(
+      files,
+      async (file: Express.Multer.File) => {
+        return isSafeFileExtension(file.filename);
+      },
+    );
+    const unsafeFiles: Array<Express.Multer.File> = files.filter(
+      (file) => !safeFiles.includes(file),
+    );
+
+    // prepare safe files to be saved
+    const filesToSave = safeFiles.map((file) => {
+      return {
+        filename: file.filename,
+        location: file.destination,
+        mimetype: file.mimetype,
+        fullpath: file.path,
+        size: file.size,
+        owner: id,
+      };
+    });
+    // remove unsafe files (files interseptor automatically save the file has an option to filter invalid file types but change extension vulnerability continues being an issue)
+    this.rentalPlaceService.removeFiles(unsafeFiles);
+    // unsafeFiles.map((file) => removeFile(file.filename));
+    // save files in mongo
+    const filesCreated = await this.imageService.createMany(filesToSave);
+    // attach them to the rental place
+    this.rentalPlaceService.update(id, { images: filesCreated });
+    if (unsafeFiles.length) {
+      return {
+        succed: files.length - unsafeFiles.length, // safe files quantity
+        error: 'bad request', // error type
+        message: `bad images ${unsafeFiles.length}`, // error message with unsafe files quantity
+        data: unsafeFiles, // unsafe files data
+      };
+    }
+    return 'files succesfully uploaded';
+  }
+
+  @Get('/images/:imagename')
+  async getFiles(@Param('imagename') imageName: string, @Res() res: Response) {
+    // return file as file
+    // return res.sendFile(imageName, { root: multerConfig.dest })
+    // return file as application/octet-stream
+    const file = createReadStream(getFullPath(imageName));
+    file.pipe(res);
   }
 }
