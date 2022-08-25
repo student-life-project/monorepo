95726381d9eaf9d678a1c02bbead8a7a4815c389
Erev14 <erevjoach14@gmail.com>
2022-08-25 16:45:46 +0000

    feat(backend): add type enums for validation

---
+++ packages/backend/src/helper/types/profile-report.ts
@@ -0,0 +1,7 @@
+export enum ProfileReport {
+  'Es irrespetuoso u ofensivo (Incita al odio)' = 'Es irrespetuoso u ofensivo (Incita al odio)',
+  'Es un perfil con información falsa' = 'Es un perfil con información falsa',
+  'Amenazar con violencia o daño físico' = 'Amenazar con violencia o daño físico',
+  'Comportamiento inapropiado' = 'Comportamiento inapropiado',
+  'Es otra cosa' = 'Es otra cosa',
+}
---
+++ packages/backend/src/helper/types/type-space.ts
@@ -0,0 +1,6 @@
+export enum TypeSpace {
+  'Lugar completo' = 'Lugar completo',
+  'Cuarto privado' = 'Cuarto privado',
+  'Cuarto compartido' = 'Cuarto compartido',
+  Otro = 'Otro',
+}
