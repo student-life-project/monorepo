a63af7bedfaa733075b683b1e22b3533285da2ed
Erev14 <erevjoach14@gmail.com>
2022-08-25 16:51:59 +0000

    feat(backend): no more needed

--- packages/backend/src/characteristic/dto/update-characteristic.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-
-import { CreateCharacteristicDto } from './create-characteristic.dto';
-
-export class UpdateCharacteristicDto extends PartialType(
-  CreateCharacteristicDto,
-) {}
