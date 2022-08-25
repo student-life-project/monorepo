4f8b37b4508a3abbf4deaeeb1c41c3ee343c7148
Erev14 <erevjoach14@gmail.com>
2022-08-25 17:03:00 +0000

    chore(backend): fix linter errors

--- packages/backend/src/rental-place/rental-place.schema.ts
+++ packages/backend/src/rental-place/rental-place.schema.ts
@@ -1,58 +1,113 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 import { Document, Schema as MongooseSchema } from 'mongoose';
 
 import { Address } from '../address/address.schema';
+// eslint-disable-next-line import/no-cycle
+import { Comment } from '../comment/comment.schema';
+import {
+  Gender,
+  Reason,
+  Rules,
+  Security,
+  Services,
+  TypeSpace,
+} from '../helper/types';
 import { Image } from '../image/image.schema';
-import { Rate } from '../rate/rate.schema';
+// eslint-disable-next-line import/no-cycle
+import { Like } from '../like/like.schema';
+
+const setPrice = (price: string) => {
+  const findDigitsAndDotRegex = /\d*\.\d{1,2}/;
+
+  price = price.replace(',', '');
+
+  const findDot = price.split(findDigitsAndDotRegex);
+
+  if (!findDot[2]) {
+    price = `${price}.0`; // HACK Adds .0 so it works with whole numbers
+  }
+
+  return parseInt((parseFloat(price) * 100).toFixed(0), 10);
+};
+
+const getPrice = (price: number) => {
+  return (price / 100).toFixed(2);
+};
 
 @Schema()
 export class RentalPlace {
+  @ApiProperty()
+  @Prop({ _id: true })
+  id?: string;
+
+  @ApiProperty()
   @Prop({ required: true })
   owner: string;
 
+  @ApiProperty()
   @Prop({ required: true })
   title: string;
 
+  @ApiProperty()
   @Prop({ required: true })
-  reason: string;
+  reason: Reason;
 
+  @ApiProperty()
   @Prop({ required: true })
-  typeSpace: string;
+  typeSpace: TypeSpace;
 
+  @ApiProperty()
   @Prop({ required: true })
-  gender: string;
+  gender: Gender;
 
-  @Prop({ required: true })
-  price: number;
+  @ApiProperty()
+  @Prop({ required: true, set: setPrice, get: getPrice })
+  price: string;
 
+  @ApiProperty()
   @Prop({ default: true })
   availability: boolean;
 
+  @ApiProperty()
   @Prop({ type: MongooseSchema.Types.ObjectId, ref: 'Address', required: true })
   address: Address;
 
+  @ApiProperty()
   @Prop({ required: true })
   description: string;
 
+  @ApiProperty()
   @Prop({ required: true })
-  services: string[];
+  services: Services[];
 
+  @ApiProperty()
   @Prop({ required: false })
-  rules: string[];
+  rules: Rules[];
 
+  @ApiProperty()
   @Prop({ required: false })
-  security: string[];
+  security: Security[];
 
+  @ApiProperty({ type: [Image] })
   @Prop([
     { type: MongooseSchema.Types.ObjectId, ref: 'Image', required: false },
   ])
-  images: Image[];
+  images?: Image[];
 
-  @Prop([{ type: MongooseSchema.Types.ObjectId, ref: 'Rate', required: false }])
-  rates: Rate[];
+  @ApiProperty({ type: [Like] })
+  @Prop([{ type: MongooseSchema.Types.ObjectId, ref: 'Like', required: false }])
+  likes?: Like[];
 
+  @ApiProperty()
   @Prop({ default: false, required: false })
   approved: boolean;
+
+  @ApiProperty({ type: [Comment] })
+  @Prop([
+    { type: MongooseSchema.Types.ObjectId, ref: 'Comment', required: false },
+  ])
+  comments?: Comment[];
 }
 
 export type RentalPlaceDocument = RentalPlace & Document;
--- packages/backend/src/rental-place/rental-place.service.ts
+++ packages/backend/src/rental-place/rental-place.service.ts
@@ -16,17 +16,19 @@ export class RentalPlaceService {
     private RentalPlaceModel: Model<RentalPlaceDocument>,
   ) {}
 
-  private readonly populateQuery = ['rates', 'images', 'address'];
-
-  removeFiles(files: Express.Multer.File[] | Image[]) {
-    files.map((file: Express.Multer.File | Image) => removeFile(file.filename));
-  }
+  private readonly populateQuery = ['likes', 'images', 'address'];
 
   async create(
     createRentalPlaceDto: CreateRentalPlaceDto,
   ): Promise<RentalPlace> {
     const createdRentalPlace = new this.RentalPlaceModel(createRentalPlaceDto);
     return createdRentalPlace.save();
+  }
+
+  async update(id: string, updateRentalPlaceDto: UpdateRentalPlaceDto) {
+    return this.RentalPlaceModel.findByIdAndUpdate(id, updateRentalPlaceDto, {
+      upsert: true,
+    });
   }
 
   async findAll(): Promise<RentalPlace[]> {
@@ -37,13 +39,103 @@ export class RentalPlaceService {
     return this.RentalPlaceModel.findById(id).populate(this.populateQuery);
   }
 
-  async update(id: string, updateRentalPlaceDto: UpdateRentalPlaceDto) {
-    return this.RentalPlaceModel.findByIdAndUpdate(id, updateRentalPlaceDto, {
-      upsert: true,
-    });
+  // TODO: fix
+  async findByOwner(id: string): Promise<RentalPlace[]> {
+    return this.RentalPlaceModel.find({ owner: id }).exec();
+  }
+
+  removeFiles(files: Express.Multer.File[] | Image[]) {
+    files.map((file: Express.Multer.File | Image) => removeFile(file.filename));
   }
 
   async remove(id: string) {
     return this.RentalPlaceModel.findByIdAndDelete(id);
+  }
+
+  async approved() {
+    return this.RentalPlaceModel.find({}).where('approved').equals(true);
+  }
+
+  async find(query: any, sort: any, paginate: any): Promise<RentalPlace[]> {
+    const INIT_PAGE = 1;
+    const q = this.RentalPlaceModel.find(query);
+
+    if (sort) {
+      q.sort(sort);
+    }
+
+    if (paginate) {
+      q.skip((paginate.page - INIT_PAGE) * paginate.limit).limit(
+        paginate.limit,
+      );
+    }
+
+    return q.populate(this.populateQuery).exec();
+  }
+
+  async count(query: any) {
+    return this.RentalPlaceModel.count(query).exec();
+  }
+
+  // TODO filters and sort
+  // ?Sort=asc/desc (price)
+  sort(sorting: string) {
+    return { sort: { price: sorting } };
+  }
+
+  // ?type=[]
+  // ?services=[”Lavadora”,,,,,]
+  // ?rules=
+  // ?from=
+  // ?limit=
+  // ?price=”less than x”/”more than x”/”between x and y”
+  getPriceFilters(price: string) {
+    const BETWEEN_SPLIT_COUNT = 4;
+    const THAN_SPLIT_COUNT = 3;
+    const BETWEEN_X_INDEX = 1;
+    const BETWEEN_Y_INDEX = 3;
+    const THAN_CRITERIA_INDEX = 0;
+    const THAN_VALUE_INDEX = 2;
+
+    const priceType = price.split(' ');
+    const typeLength = priceType.length;
+    console.log('priceType', priceType);
+    console.log('typeLength', typeLength);
+
+    if (typeLength !== THAN_SPLIT_COUNT && typeLength !== BETWEEN_SPLIT_COUNT) {
+      return {};
+    }
+
+    if (typeLength === BETWEEN_SPLIT_COUNT) {
+      return {
+        $lte: priceType[BETWEEN_X_INDEX],
+        $gte: priceType[BETWEEN_Y_INDEX],
+      };
+    }
+
+    if (priceType[THAN_CRITERIA_INDEX] === 'less') {
+      return { $lte: priceType[THAN_VALUE_INDEX] };
+    }
+    if (priceType[THAN_CRITERIA_INDEX] === 'more') {
+      return { $gte: priceType[THAN_VALUE_INDEX] };
+    }
+    return {};
+  }
+
+  priceFilter(query: any, price: string) {
+    const priceCriteria = this.getPriceFilters(price);
+    if (priceCriteria) {
+      return { ...query, price: priceCriteria };
+    }
+    return query;
+  }
+
+  async createMany(
+    createRentalPlaceDto: CreateRentalPlaceDto[],
+  ): Promise<RentalPlace[]> {
+    const createdRentalPlace = await this.RentalPlaceModel.insertMany(
+      createRentalPlaceDto,
+    );
+    return createdRentalPlace;
   }
 }
