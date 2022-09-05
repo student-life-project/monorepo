3d98d461c5e1f4051dd7222c6b071448f4f78568
AlfredoCU <alfredo_11_cu@hotmail.com>
2022-09-04 23:41:50 -0500

    feat: action comment created

--- packages/frontend/src/store/actions/comment.ts
+++ packages/frontend/src/store/actions/comment.ts
@@ -5,15 +5,20 @@ import { ThunkAction } from 'redux-thunk';
 import { AlertMessage } from '@/constants/alertMessage';
 import { TRootState } from '@/store/reducers';
 import {
-  // CREATE_COMMENT,
-  // DELETE_COMMENT,
-  // GET_ALL_COMMENT_PENDING,
-  GET_ALL_COMMENTS,
-  // GET_COMMENT,
+  CREATE_COMMENT_ERROR,
+  CREATE_COMMENT_PENDING,
+  CREATE_COMMENT_SUCCESS,
+  DELETE_COMMENT_ERROR,
+  DELETE_COMMENT_SUCCESS,
+  GET_ALL_COMMENTS_ERROR,
+  GET_ALL_COMMENTS_PENDING,
+  GET_ALL_COMMENTS_SUCCESS,
   GET_COMMENT_ERROR,
   GET_COMMENT_PENDING,
   GET_COMMENT_SUCCESS,
-  // UPDATE_COMMENT,
+  UPDATE_COMMENT_ERROR,
+  UPDATE_COMMENT_PENDING,
+  UPDATE_COMMENT_SUCCESS,
 } from '@/store/types/comment';
 import { IQueryCommonFilters, TElementId } from '@/types';
 
@@ -39,10 +44,11 @@ export const getComment =
     try {
       dispatch(getCommentPendingAction());
 
+      // TODO: Eliminar
       // const { data } = await api.get(`/comment/${id}`);
       const data = {};
       // eslint-disable-next-line no-console
-      console.log(id); // TODO: Eliminar
+      console.log(id);
 
       dispatch(getCommnetSuccessAction(data));
     } catch (error) {
@@ -53,30 +59,154 @@ export const getComment =
 
 // =============================================================================
 
-export const getAllComment =
-  ({ limit }: IQueryCommonFilters = {}) =>
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const createCommentPendingAction = (): any => ({
+  type: CREATE_COMMENT_PENDING,
+});
+
+export const createCommentSuccessAction = (data: unknown): any => ({
+  type: CREATE_COMMENT_SUCCESS,
+  data,
+});
+
+export const createCommentErrorAction = (error: AxiosError): any => ({
+  type: CREATE_COMMENT_ERROR,
+  error,
+});
+
+export const createComment =
+  (comment: unknown): ThunkAction<void, TRootState, unknown, any> =>
   async (dispatch) => {
     try {
-      // const limitQuery = limit ? `?limit=${limit}` : '';
-      // const { data } = await api.get(`/comment${limitQuery}`);
+      dispatch(createCommentPendingAction());
+      // const { data } = await api.post('/create', { comment });
+
+      // TODO: Eliminar
       const data = {};
       // eslint-disable-next-line no-console
-      console.log(limit); // TODO: Eliminar
+      console.log(comment);
 
-      dispatch({ type: GET_ALL_COMMENTS, data });
+      dispatch(createCommentSuccessAction(data));
+      toast.success(AlertMessage.created('comentario'));
     } catch (error) {
-      console.error(error);
+      dispatch(createCommentErrorAction(error));
+      toast.error(AlertMessage.error);
     }
   };
 
 // =============================================================================
 
-/*
-    const { data } = await api.post<ILoginResponse>('/login', {
-      email: credentials.email,
-      password: credentials.password,
-    });
-    */
+export const updateCommentPendingAction = (): any => ({
+  type: UPDATE_COMMENT_PENDING,
+});
+
+export const updateCommentSuccessAction = (data: unknown): any => ({
+  type: UPDATE_COMMENT_SUCCESS,
+  data,
+});
+
+export const updateCommentErrorAction = (error: AxiosError): any => ({
+  type: UPDATE_COMMENT_ERROR,
+  error,
+});
+
+export const updateComment =
+  (
+    id: TElementId,
+    comment: unknown,
+  ): ThunkAction<void, TRootState, unknown, any> =>
+  async (dispatch) => {
+    try {
+      dispatch(createCommentPendingAction());
+      // const { data } = await api.put(`/update/${id}`, { comment });
+
+      // TODO: Eliminar
+      const data = {};
+      // eslint-disable-next-line no-console
+      console.log(id, comment);
+
+      dispatch(createCommentSuccessAction(data));
+      toast.success(AlertMessage.updated('comentario'));
+    } catch (error) {
+      dispatch(createCommentErrorAction(error));
+      toast.error(AlertMessage.error);
+    }
+  };
+
+// =============================================================================
+
+export const deleteCommentPendingAction = (): any => ({
+  type: DELETE_COMMENT_SUCCESS,
+});
+
+export const deleteCommentSuccessAction = (data: unknown): any => ({
+  type: DELETE_COMMENT_SUCCESS,
+  data,
+});
+
+export const deleteCommentErrorAction = (error: AxiosError): any => ({
+  type: DELETE_COMMENT_ERROR,
+  error,
+});
+
+export const deleteComment =
+  (id: TElementId): ThunkAction<void, TRootState, unknown, any> =>
+  async (dispatch) => {
+    try {
+      dispatch(createCommentPendingAction());
+      // const { data } = await api.delete(`/delete/${id}`);
+
+      // TODO: Eliminar
+      const data = {};
+      // eslint-disable-next-line no-console
+      console.log(id);
+
+      dispatch(createCommentSuccessAction(data));
+      toast.success(AlertMessage.deleted('comentario'));
+    } catch (error) {
+      dispatch(createCommentErrorAction(error));
+      toast.error(AlertMessage.error);
+    }
+  };
+
+// =============================================================================
+
+export const getAllCommentsPendingAction = (): any => ({
+  type: GET_ALL_COMMENTS_PENDING,
+});
+
+export const getAllCommentsSuccessAction = (data: unknown): any => ({
+  type: GET_ALL_COMMENTS_SUCCESS,
+  data,
+});
+
+export const getAllCommentsErrorAction = (error: AxiosError): any => ({
+  type: GET_ALL_COMMENTS_ERROR,
+  error,
+});
+
+export const getAllComments =
+  ({ limit }: IQueryCommonFilters = {}): ThunkAction<
+    void,
+    TRootState,
+    unknown,
+    any
+  > =>
+  async (dispatch) => {
+    try {
+      dispatch(getAllCommentsPendingAction());
+      // const limitQuery = limit ? `?limit=${limit}` : '';
+      // const { data } = await api.get(`/comment${limitQuery}`);
+
+      // TODO: Eliminar
+      const data = {};
+      // eslint-disable-next-line no-console
+      console.log(limit);
+
+      dispatch(getAllCommentsSuccessAction(data));
+    } catch (error) {
+      dispatch(getAllCommentsErrorAction(error));
+      toast.error(AlertMessage.error);
+    }
+  };
 
 // =============================================================================
