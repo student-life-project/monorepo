a62d02956237145fb26ae7cddb265ea773108e41
AlfredoCU <alfredo_11_cu@hotmail.com>
2022-09-06 22:43:59 -0500

    feat: report action created

---
+++ packages/frontend/src/store/actions/report.ts
@@ -0,0 +1,52 @@
+import { AxiosError } from 'axios';
+import { toast } from 'react-toastify';
+import { ThunkAction } from 'redux-thunk';
+
+import { AlertMessage } from '@/constants/alertMessage';
+import { TRootState } from '@/store/reducers';
+import {
+  CREATE_REPORT_ERROR,
+  CREATE_REPORT_PENDING,
+  CREATE_REPORT_SUCCESS,
+} from '@/store/types/report';
+
+export const createReportPendingAction = (): any => ({
+  type: CREATE_REPORT_PENDING,
+});
+
+export const createReportSuccessAction = (data: unknown): any => ({
+  type: CREATE_REPORT_SUCCESS,
+  data,
+});
+
+export const createReportErrorAction = (error: AxiosError): any => ({
+  type: CREATE_REPORT_ERROR,
+  error,
+});
+
+export const createReport =
+  (report: unknown): ThunkAction<void, TRootState, unknown, any> =>
+  async (dispatch) => {
+    try {
+      dispatch(createReportPendingAction());
+      // const { data } = await api.post('/report', { report });
+
+      // TODO: Eliminar
+      const data = {};
+      // eslint-disable-next-line no-console
+      console.log(report);
+
+      dispatch(createReportSuccessAction(data));
+
+      // let alertType = 'reporte de usuario';
+
+      // if (type === ERentalType.RENTAL_PLACE) {
+      //   alertType = 'reporte de alojamiento';
+      // }
+
+      toast.success(AlertMessage.created('reporte')); // alertType
+    } catch (error) {
+      dispatch(createReportErrorAction(error));
+      toast.error(AlertMessage.error);
+    }
+  };
