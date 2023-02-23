107988addca7f4ce1c560120af7f39f4dde3866f
Alfredo Carreón Urbano <42507426+AlfredoCU@users.noreply.github.com>
2023-02-22 20:45:33 -0600

    feat/camara (#45)
    * feat: modal step facial recognition
    
    * feat: camara component created
    
    * feat: camara component styles and copies updated
    
    * refactor: copie updated

--- packages/frontend/src/components/profile/UpdateUser.tsx
+++ packages/frontend/src/components/profile/UpdateUser.tsx
@@ -1,24 +1,28 @@
 /* eslint-disable simple-import-sort/imports */
 import xw from 'xwind';
 import styled from '@emotion/styled';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import {
+  faChevronRight,
+  faIdCard,
+  faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { EUserType } from '@student_life/common';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
-// import { toast } from 'react-toastify';
 
+// import { toast } from 'react-toastify';
 import Button from '@/components/common/Button';
+import WebcamImage from '@/components/common/Camara';
 import DoubleFormSpace from '@/components/common/DoubleFormSpace';
 import Input from '@/components/common/Input';
 import Label from '@/components/common/Label';
+import Modal from '@/components/common/Modal';
 import Radio from '@/components/common/Radio';
 import Tooltip from '@/components/common/Tooltip';
 import { ErrorMessageInput, NameInput } from '@/constants';
 // import { AlertMessage } from '@/constants/alertMessage';
 import { calculateAge } from '@/utils/managerDate';
-
-import Modal from '../common/Modal';
 
 interface IRegisterData {
   userType: EUserType;
@@ -65,18 +69,31 @@ type TUpdateUser = {
 // TODO: si se registran con gmail, cargar en nombre y el apellido.
 const UpdateUser: React.FC<TUpdateUser> = ({ closeModal }) => {
   const {
-    handleSubmit,
-    register,
-    control,
     reset,
+    control,
+    trigger,
+    register,
+    handleSubmit,
     formState: { errors },
   } = useForm({
     mode: 'all',
   });
 
-  useEffect(() => {
-    reset({ userType: EUserType.STUDENT });
-  }, [reset]);
+  const [faceImage, setFaceImage] = useState(null);
+  const [showFaceImage, setShowFaceImage] = useState(false);
+
+  const [idCardImage, setIdCardImage] = useState(null);
+  const [showIdCardImage, setShowIdCardImage] = useState(false);
+
+  const [facialRecognition, setFacialRecognition] = useState(false);
+
+  const handleShowFaceImage = () => {
+    setShowFaceImage(!showFaceImage);
+  };
+
+  const handleShowIdCardImage = () => {
+    setShowIdCardImage(!showIdCardImage);
+  };
 
   const onSubmit: SubmitHandler<IRegisterData> = async (data) => {
     // eslint-disable-next-line no-console
@@ -85,133 +102,223 @@ const UpdateUser: React.FC<TUpdateUser> = ({ closeModal }) => {
     closeModal(); // TODO: cerrar si el resultado es success
   };
 
+  const previousStep = () => {
+    setFacialRecognition(false);
+  };
+
+  const nextStep = async () => {
+    const result = await trigger();
+
+    if (result) {
+      setFacialRecognition(true);
+    }
+  };
+
+  useEffect(() => {
+    reset({ userType: EUserType.STUDENT });
+  }, [reset]);
+
   return (
     <Modal>
       <form onSubmit={handleSubmit(onSubmit)}>
         <Title>Te damos la bienvenida</Title>
-        <Text>
-          Es importante que ingreses la siguiente información para personalizar
-          tu perfil
-        </Text>
 
-        <Label id="label-first-name" htmlFor="first-name">
-          {NameInput.userRole}
-        </Label>
-        <DoubleFormSpace>
-          <Controller
-            name="userType"
-            control={control}
-            rules={{ required: true }}
-            render={({ field: { onChange, value } }) => {
-              const role = value as EUserType;
+        {!facialRecognition ? (
+          <>
+            <Text>
+              Es importante que ingreses la siguiente información para
+              personalizar tu perfil
+            </Text>
 
-              return (
-                <>
-                  <RadioContainer data-tip data-for="student">
-                    <>
-                      <Radio
-                        name="student"
-                        label="Estudiante"
-                        value={EUserType.STUDENT}
-                        onChange={onChange}
-                        checked={role === EUserType.STUDENT}
-                      />
-
-                      <Tooltip id="student" type="info">
-                        <span>Buscar alojamientos</span>
-                      </Tooltip>
-                    </>
-                  </RadioContainer>
-
-                  <RadioContainer data-tip data-for="renter">
-                    <>
-                      <Radio
-                        name="renter"
-                        label="Arrendatario"
-                        value={EUserType.OWNER}
-                        onChange={onChange}
-                        checked={role === EUserType.OWNER}
-                      />
-
-                      <Tooltip id="renter" type="info">
-                        <span>Buscar inquilino o buscar un rommie</span>
-                      </Tooltip>
-                    </>
-                  </RadioContainer>
-                </>
-              );
-            }}
-          />
-
-          <div css={xw`mb-4`}>
             <Label id="label-first-name" htmlFor="first-name">
-              {NameInput.firstName}
+              {NameInput.userRole}
             </Label>
-            <Input
-              type="text"
-              id="first-name"
-              placeholder="Tu nombre"
-              register={{
-                ...register('firstName', {
-                  required: ErrorMessageInput.inputRequire(NameInput.firstName),
-                }),
-              }}
-              error={errors.firstName}
-              messageError={errors.firstName?.message}
-            />
-          </div>
+            <DoubleFormSpace>
+              <Controller
+                name="userType"
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { onChange, value } }) => {
+                  const role = parseInt(value, 10);
 
-          <div css={xw`mb-4`}>
-            <Label id="label-last-name" htmlFor="last-name">
-              {NameInput.lastName}
-            </Label>
-            <Input
-              type="text"
-              id="last-name"
-              placeholder="Tu apellido"
-              register={{
-                ...register('lastName', {
-                  required: ErrorMessageInput.inputRequire(NameInput.lastName),
-                }),
-              }}
-              error={errors.lastName}
-              messageError={errors.lastName?.message}
-            />
-          </div>
-        </DoubleFormSpace>
+                  return (
+                    <>
+                      <RadioContainer data-tip data-for="student">
+                        <>
+                          <Radio
+                            name="student"
+                            label="Estudiante"
+                            value={EUserType.STUDENT}
+                            onChange={onChange}
+                            checked={role === EUserType.STUDENT}
+                          />
 
-        <div css={xw`mb-4`}>
-          <Label id="label-birth-date" htmlFor="birth-date">
-            {NameInput.birthDate}
-          </Label>
-          <Input
-            type="date"
-            id="birth-date"
-            placeholder="Tu fecha de nacimiento"
-            register={{
-              ...register('birthDate', {
-                required: ErrorMessageInput.inputRequire(NameInput.birthDate),
-                validate: (value) =>
-                  calculateAge(value) >= 18 || ErrorMessageInput.ageValid,
-              }),
-            }}
-            error={errors.birthDate}
-            messageError={errors.birthDate?.message}
-          />
+                          <Tooltip id="student" type="info">
+                            <span>Buscar alojamientos</span>
+                          </Tooltip>
+                        </>
+                      </RadioContainer>
+
+                      <RadioContainer data-tip data-for="renter">
+                        <>
+                          <Radio
+                            name="renter"
+                            label="Arrendatario"
+                            value={EUserType.OWNER}
+                            onChange={onChange}
+                            checked={role === EUserType.OWNER}
+                          />
+
+                          <Tooltip id="renter" type="info">
+                            <span>Buscar inquilino o buscar un rommie</span>
+                          </Tooltip>
+                        </>
+                      </RadioContainer>
+                    </>
+                  );
+                }}
+              />
+
+              <div css={xw`mb-4`}>
+                <Label id="label-first-name" htmlFor="first-name">
+                  {NameInput.firstName}
+                </Label>
+                <Input
+                  type="text"
+                  id="first-name"
+                  placeholder="Tu nombre"
+                  register={{
+                    ...register('firstName', {
+                      required: ErrorMessageInput.inputRequire(
+                        NameInput.firstName,
+                      ),
+                    }),
+                  }}
+                  error={errors.firstName}
+                  messageError={errors.firstName?.message}
+                />
+              </div>
+
+              <div css={xw`mb-4`}>
+                <Label id="label-last-name" htmlFor="last-name">
+                  {NameInput.lastName}
+                </Label>
+                <Input
+                  type="text"
+                  id="last-name"
+                  placeholder="Tu apellido"
+                  register={{
+                    ...register('lastName', {
+                      required: ErrorMessageInput.inputRequire(
+                        NameInput.lastName,
+                      ),
+                    }),
+                  }}
+                  error={errors.lastName}
+                  messageError={errors.lastName?.message}
+                />
+              </div>
+            </DoubleFormSpace>
+
+            <div css={xw`mb-4`}>
+              <Label id="label-birth-date" htmlFor="birth-date">
+                {NameInput.birthDate}
+              </Label>
+              <Input
+                type="date"
+                id="birth-date"
+                placeholder="Tu fecha de nacimiento"
+                register={{
+                  ...register('birthDate', {
+                    required: ErrorMessageInput.inputRequire(
+                      NameInput.birthDate,
+                    ),
+                    validate: (value) =>
+                      calculateAge(value) >= 18 || ErrorMessageInput.ageValid,
+                  }),
+                }}
+                error={errors.birthDate}
+                messageError={errors.birthDate?.message}
+              />
+            </div>
+
+            <p css={xw`text-center sm:text-left`}>
+              Al registrarte aceptas nuestros&nbsp;
+              <span css={xw`font-bold`}>términos y condiciones</span> y la&nbsp;
+              <span css={xw`font-bold`}>política de privacidad</span>.
+            </p>
+          </>
+        ) : (
+          <>
+            <Text>
+              Para validar tu identidad toma una foto de tu identificación
+              oficial y una foto de tu rostro.
+            </Text>
+
+            <p css={xw`text-center`}>
+              <span css={xw`font-bold text-primary`}> Recomendación:</span> toma
+              tus fotos con buena iluminación
+            </p>
+
+            <div css={xw`flex justify-center flex-wrap gap-10 my-10`}>
+              <WebcamImage
+                icon={faIdCard}
+                img={idCardImage}
+                setImg={setIdCardImage}
+                showCam={showIdCardImage}
+                onShowCam={handleShowIdCardImage}
+                description="Foto de frente y de forma horizontal de tu identificación oficial"
+              />
+
+              <WebcamImage
+                icon={faUser}
+                img={faceImage}
+                setImg={setFaceImage}
+                showCam={showFaceImage}
+                onShowCam={handleShowFaceImage}
+                description="Foto de tu rostro sin accesorios (lentes, gorras, cubrebocas, etc.)"
+              />
+            </div>
+          </>
+        )}
+
+        <div
+          css={xw`flex justify-end flex-col-reverse sm:flex-row flex-wrap my-4`}
+        >
+          {!facialRecognition ? (
+            <>
+              <Button
+                FPrimary
+                type="button"
+                onClick={nextStep}
+                css={xw`w-full sm:w-3/12 mb-5 sm:mb-0`}
+              >
+                <span css={xw`mr-2`}>Continuar</span>
+                <FontAwesomeIcon icon={faChevronRight} height=".875rem" />
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button
+                BSecondary
+                type="button"
+                onClick={previousStep}
+                css={xw`w-full sm:w-3/12 mb-5 sm:mr-5 sm:mb-0`}
+              >
+                Regresar
+              </Button>
+
+              <Button
+                FPrimary
+                type="submit"
+                css={xw`w-full sm:w-3/12 mb-5 sm:mb-0`}
+              >
+                <span css={xw`mr-2`}>Regístrate</span>
+                <FontAwesomeIcon icon={faChevronRight} height=".875rem" />
+              </Button>
+            </>
+          )}
         </div>
-
-        <div css={xw`my-4`}>
-          <Button type="submit" FPrimary css={xw`w-full`}>
-            <span css={xw`mr-2`}>Regístrate</span>
-            <FontAwesomeIcon icon={faChevronRight} height=".875rem" />
-          </Button>
-        </div>
-
-        <p css={xw`text-center sm:text-left`}>
-          Al registrarte aceptas nuestros&nbsp;
-          <span css={xw`font-bold`}>Términos y condiciones</span> y la&nbsp;
-          <span css={xw`font-bold`}>Política de privacidad</span>.
-        </p>
       </form>
     </Modal>
   );
