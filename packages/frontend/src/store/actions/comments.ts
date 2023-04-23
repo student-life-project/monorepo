64c6bfb98551da85b588f531765e012aa9de67f2
AlfredoCU <alfredo_11_cu@hotmail.com>
2023-04-22 21:54:19 -0600

    refactor: todos

--- packages/frontend/src/store/actions/comments.ts
+++ packages/frontend/src/store/actions/comments.ts
@@ -40,7 +40,7 @@ export const getCommentErrorAction = (error: AxiosError): any => ({
   error,
 });
 
-// TODO: No regresa la data
+//! No regresa la data
 export const getComment =
   (
     id: TElementId,
