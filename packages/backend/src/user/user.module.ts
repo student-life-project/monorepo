57a4dd7e593a39a380e387c8faf58d5eac6717b9
Erick Mejia Blanco <erickblanc96@gmail.com>
2023-04-27 20:35:31 -0600

    fix(backend): fixed admin endpoints due to the lack of pagination controllers (#99)
    Co-authored-by: Erick Mejia <erickblsnc96@gmail.com>

--- packages/backend/src/user/user.module.ts
+++ packages/backend/src/user/user.module.ts
@@ -4,11 +4,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { ImageModule } from '../image/image.module';
 import { ImageService } from '../image/image.service';
+import { PaginationModule } from '../pagination/pagination.module';
 import { UserController } from './user.controller';
-// import { MessageModule } from '../message/message.module';
-// import { OwnerModule } from '../owner/owner.module';
-// import { ReportModule } from '../report/report.module';
-// import { StudentModule } from '../student/student.module';
 import { User, UserSchema } from './user.schema';
 import { UserService } from './user.service';
 
@@ -16,11 +13,8 @@ import { UserService } from './user.service';
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     ImageModule,
-    // MessageModule,
-    // OwnerModule,
-    // ReportModule,
-    // StudentModule,
     HttpModule,
+    PaginationModule,
   ],
   exports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
